6415ecc93beee0c699f705b8c465858ee169c3bd
Tony Chow <1020466980@qq.com>
2019-07-31 22:50:47 +0800

    first commit

---
+++ KNN.py
@@ -0,0 +1,28 @@
+#-*- coding:utf-8 -*-
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+
+iris=datasets.load_iris()
+iris_x=iris.data
+iris_y=iris.target
+
+indices=np.random.permutation(len(iris_x))
+iris_x_train=iris_x[indices[:-10]]
+iris_y_train=iris_y[indices[:-10]]
+
+iris_x_test=iris_x[indices[-10:]]
+iris_y_test=iris_y[indices[-10:]]
+
+knn=KNeighborsClassifier()
+knn.fit(iris_x_train,iris_y_train)
+iris_y_predict=knn.predict(iris_x_test)
+score=knn.score(iris_x_test,iris_y_test,sample_weight=None)
+
+print('iris_y_predict= ')
+print(iris_y_predict)
+print('iris_y_test= ')
+print(iris_y_test)
+
+print 'Accuracy:', score
+
---
+++ 随机森林原理.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+
+iris=datasets.load_iris()
+iris_x=iris.data
+iris_y=iris.target
+
+indices=np.random.permutation(len(iris_x))
+x_train=iris_x[indices[:-10]]
+y_train=iris_y[indices[:-10]]
+x_test=iris_x[indices[-10:]]
+y_test=iris_y[indices[-10:]]
+
+clfs={'random_forest' : RandomForestClassifier(n_estimators=50)}
+
+def try_different_method(clf):
+    clf.fit(x_train,y_train.ravel())
+    score=clf.score(x_test,y_test.ravel())
+    print('the score is :', score)
+
+for clf_key in clfs.keys():
+    print('the classifier is :',clf_key)
+    clf=clfs[clf_key]
+    try_different_method(clf)
